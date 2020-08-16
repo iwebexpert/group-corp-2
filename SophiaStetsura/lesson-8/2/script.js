f76e3a1345609e72500084308fa05e4fac72354a
sophie lamperg <vasilevna-sofiya@mail.ru>
2020-08-16 18:06:43 +0300

    final8

--- SophiaStetsura/lesson-8/2/script.js
+++ SophiaStetsura/lesson-8/2/script.js
@@ -1,68 +1,156 @@
-// document.addEventListener('DOMContentLoaded', () => {
-let main = document.querySelector('.main');
-let hd = document.createElement('h1');
-hd.className = 'header';
-hd.innerHTML = 'This is your basket';
-hd.style.textAlign = 'center';
-main.appendChild(hd);
-let basketWrap = document.createElement('div');
-basketWrap.className = 'basket__wrap';
-main.appendChild(basketWrap);
-let newProd;
-
-// class Basket {
-//     constructor(prod, quan, price, url) {
-//         this.prod = prod;
-//         this.quan = quan;
-//         this.price = price;
-//         this.url = url;
-//     }
-//     getPriceProd() {
-//         return this.price * this.quan;
-//     }
-// }
-
-function createProdItem(prod, price, quan) {
+ document.addEventListener('DOMContentLoaded', () => {
+  let main = document.querySelector('.main');
+  let hd = document.createElement('h1');
+  hd.className = 'header';
+  hd.innerHTML = 'This is your basket';
+  hd.style.textAlign = 'center';
+  main.appendChild(hd);
+  let basketWrap = document.createElement('div');
+  basketWrap.className = 'basket__wrap';
+  main.appendChild(basketWrap);
+  let newProd;
+  
+  class Basket {
+    constructor(prod, price) {
+      this.prod = prod;
+      this.price = price;
+      this.sum={
+        'Общая сумма': 0,
+        'Количество товара': 0,
+        Товар: this.prod
+      }
+    }
+    getSum(prod, sum, amount) {
+        this.sum={
+        'Общая сумма': sum,
+        'Количество товара': amount,
+        Товар: prod
+      };
+    }
+    returnSum(){
+      return this.sum;
+    }
+    getItemName() {
+      return this.prod;
+    }
+  }
+  function createProdItem(item) {
     newProd = document.createElement('div');
     newProd.className = 'prod__wrap';
     newProd.innerHTML = `
- <div class="prod__header">${prod}</div>
- <div class="prod__price">${price}</div>
- <button class="buy">BUY</button>
- <button class="plus">+</button>
- <p class="quan">${quan}</p>
- <button class="minus">-</button>
- <button class="clear">DELETE</button>`;
+   <div class="prod__header">${item.prod}</div>
+   
+   <div class="count">
+   <div class="prod__price">${item.price}</div>
+   <button class="minus">-</button>
+   <div class="number">0</div>
+   <button class="plus">+</button>
+  </div>`;
     basketWrap.appendChild(newProd);
-}
-createProdItem("pizza", 1500, 0);
-createProdItem("sandwich", 120, 0);
-createProdItem("ice", 100, 0);
-function sumProd(price, quan) {
-    return this.price * this.quan;
-}
-// let basket = document.createElement('div');
-// basket.className = 'basket';
-// main.appendChild(basket);
-// let sum = document.createElement('div');
-// sum.className = 'sumBasket';
-// sum.appendChild
+  }
+  let addBasket = document.createElement('button');
+  addBasket.className = 'add__btn';
+  addBasket.innerHTML = 'Купить';
+  main.appendChild(addBasket);
+  
+  // ------------------------------------------
+  let basketInfo = document.createElement('div');
+  basketInfo.className = 'basket__info';
+  basketInfo.innerHTML =
+    `<div class="sum__wrap">Корзина пуста</div>
+     <div class="sum__price"></div>`;
+  main.appendChild(basketInfo);
+  
+  let newFood1 = new Basket("pizza", 1500);
+  let newFood2 = new Basket("sandwich", 120);
+  let newFood3 = new Basket("ice", 100);
+  let arrFood = [newFood1, newFood2, newFood3];
+  createProdItem(newFood1);
+  createProdItem(newFood2);
+  createProdItem(newFood3);
+  // -------------------------------------------------------
+  let del = document.createElement('button');
+  del.className = 'delete';
+  del.innerHTML = 'Очистить';
+  main.appendChild(del);
+  // -----------------------------------------------
+  
+  // -----------------------------------------------------
+  var infoWrap = document.querySelector('.sum__wrap');
+  //  ----------------------plus and minus--------------------
+  function addHandlers(count) {
+    var minus = count.querySelector(".minus");
+    var number = count.querySelector(".number");
+    var plus = count.querySelector(".plus");
+    let price = count.querySelector('.prod__price');
 
-// -----------------------------------------------
-let buy = document.querySelectorAll('.buy');
-let clear = document.querySelectorAll('.clear');
-let plus = document.querySelectorAll('.plus');
-let minus = document.querySelectorAll('.minus');
-let quan = document.querySelectorAll('.quan');
-let info = document.querySelector('.basket__info');
-let sum = document.querySelector('.sum__price');
+    plus.addEventListener("click", function () {
+     number.innerText++;
+      sum = number.innerText * price.innerText; 
+      for (let i=0; i<arrFood.length; i++)
+        {
+          if (arrFood[i].getItemName() === count.previousElementSibling.textContent)
+            {
+              arrFood[i].getSum(arrFood[i].getItemName(), sum, parseInt(number.innerText));
+            }
+        }
+    });
+    minus.addEventListener("click", function () {
+      if (number.textContent == 0) {
+        number.textContent = 0;
+      }
+      else {
+        number.innerText--;
+      }
+      sum = number.innerText * price.innerText; 
+      for (let i=0; i<arrFood.length; i++)
+        {
+          if (arrFood[i].getItemName() === count.previousElementSibling.textContent)
+            {
+              arrFood[i].getSum(arrFood[i].getItemName(), sum, parseInt(number.innerText));
+            }
+        }
+    });
+  }
+  
+  var counts = document.querySelectorAll(".count");
+  counts.forEach(addHandlers);
 
-
-plus.addEventListener('click', plusBtn);
-let basketInfo = document.createElement('div');
-basketInfo.className = 'basket__info';
-basketInfo.innerHTML =
-    `<div class="basket__info"></div>
-<div class="sum__price"></div>`;
-main.appendChild(basketInfo);
-
+  // -------------------clear--------------------------------
+  let dele = document.querySelector('.delete');
+  
+  dele.addEventListener("click", function () {
+    var number = document.querySelectorAll(".number");
+    for (let i=0; i<arrFood.length; i++)
+    {
+      arrFood[i].getSum(arrFood[i].getItemName(), 0, 0);
+    }
+    infoWrap.textContent = 'Корзина пуста';
+    for (let i = 0; i < number.length; i++) {
+      number[i].innerText = 0;
+    }
+  })
+  
+  let price = document.querySelectorAll('.prod__price');
+  var number = document.querySelectorAll(".number");
+  addBasket.addEventListener("click", function () {
+    let infoPrice=0;
+    let infoAmount='';
+    for (let i=0; i<arrFood.length; i++)
+      {
+        if (arrFood[i].returnSum()['Количество товара'] !== 0)
+        {
+            infoAmount+=`${arrFood[i].returnSum()['Товар']} в количестве ${arrFood[i].returnSum()['Количество товара']} штук `;
+            infoPrice+=arrFood[i].returnSum()['Общая сумма'];
+        }
+      }
+    if (infoPrice === 0)
+      {
+        infoWrap.innerText = `В корзине ничего нет`;
+      }
+    else {
+        infoWrap.innerText = `В корзине выбрано ${infoAmount} на общую сумму в ${infoPrice} рублей`;
+    }
+      infoAmount='';
+  })
+});

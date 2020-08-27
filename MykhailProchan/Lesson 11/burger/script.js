2341d7d98f0c43a69d4d38f8e9b390bafd01e5e6
Misha <mpeaches@live.com>
2020-08-27 07:35:33 +0300

    burger

--- MykhailProchan/Lesson 11/burger/script.js
+++ MykhailProchan/Lesson 11/burger/script.js
@@ -1,6 +1,31 @@
-function formAction() {
-  const data = serialize(form)
+const lib = {
+  big: ['Большой', 100, 40],
+  small: ['Маленький', 50, 20],
+  cheese: ['С сыром', 10, 20],
+  salad: ['С салатом', 20, 5],
+  potato: ['С картофелем', 15, 10],
+  flavoring: ['С приправой', 15, 0],
+  mayo: ['С майонезом', 20, 5]
+}
 
+function formAction() {
+
+  const data = serialize(form);
+  let sum = 0, kcal = 0;
+  const kcalElement = document.querySelector('#kcal');
+  const sumElement = document.querySelector('#sum');
+  const checkElement = document.querySelector('#check');
+  data.forEach(a => {
+    const temp = lib[Object.keys(lib).find(key => a[1] == key)];
+    console.log(temp);
+    checkElement.appendChild(document.createElement('p')).textContent = temp[0] +
+      ' ' + temp[1] + ' руб';
+    sum += temp[1];
+    kcal += temp[2];
+  })
+
+  kcalElement.textContent = 'Калории: ' + kcal + ' ккал';
+  sumElement.textContent = 'Стоимость: ' + sum + ' руб';
 }
 
 function serialize(form) {

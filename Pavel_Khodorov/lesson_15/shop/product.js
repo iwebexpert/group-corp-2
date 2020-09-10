dc1f0ef0570bd3286a5050b463633061e47f0c39
Павел <hodorovpavel@gmail.com>
2020-09-10 10:26:03 +0300

    lesson_16

--- Pavel_Khodorov/lesson_15/shop/product.js
+++
@@ -1,9 +0,0 @@
-class Product {
-    constructor(name, price, quantity, images) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.images = images;
-
-    }
-}

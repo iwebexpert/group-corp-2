9629399295ef9a3752727ac1d97fe78e971491af
Eugene Shevchenko <imsorler@gmail.com>
2020-09-10 11:25:39 +0300

    burger

--- EugeneShevchenko/lesson12/burger/index.js
+++ EugeneShevchenko/lesson12/burger/index.js
@@ -3,50 +3,42 @@
 document.addEventListener('DOMContentLoaded', () => {
   class Menu {
     constructor(size) {
-      this.size = {
-        s1: 'small',
-        s2: 'large',
-      }
+      this.size = size
     }
   }
   let priceWithoutToppings = 0;
   class Hamburger extends Menu {
-    constructor(size, price, myToppings, parentSelector) {
-      super();
-      this.size = size;
-      this.price = price;
-      this.myToppings = {};
-      this.parent = document.querySelector('#result');
+    constructor(size, price, calories, myToppings, parentSelector) {
+      super()
+      this.size = this.checkSize()
+      this.price = price
+      this.calories = calories
+      this.myToppings = {}
+      this.parent = document.querySelector('#result')
     }
-    checkPrice() {
-      const size = document.querySelectorAll('input[type="radio"]');
+    checkSize() {
+      const size = document.querySelectorAll('input[type="radio"]')
       size.forEach(radio => {
         radio.addEventListener('click', () => {
           if (radio.value == 'small') {
-            this.price = 200;
-            this.calories = 140;
+                   this.calories = 200
+                   this.price = 100
+            return this.size = 'small'
           } else if (radio.value == 'large') {
-            this.price = 500;
-            this.calories = 395;
+                   this.calories = 450
+                   this.price = 250
+            return this.size = 'large'
           }
         })
       })
     }
-    render() {
-          const element = document.createElement('div');
-          element.innerHTML = `
-      			<div><b>Стоимость:</b> <span id="price">${this.price}</span> руб.</div>
-      			<div><b>Калорийность:</b> <span id="calories">${this.calories}</span>ккал.</div>
-          `;
-          this.parent.append(element);
-      	}
   }
 
-  let hamburger1 = new Hamburger(new Menu().size.s1)
+  let hamburger1 = new Hamburger()
   console.log(hamburger1)
-  
+
   const menu1 = new Menu()
-  // console.log(menu1.size.s1)
+  console.log(menu1)
 
   let someObj = {}; // needed to collect object as property
   let someTempObj = {}; // needed to collect properties
@@ -90,19 +82,19 @@ document.addEventListener('DOMContentLoaded', () => {
             for (var current in filling.toppings) {
               // console.log(filling.toppings[current]) // all current
               if (item.id == current) {
-                console.log(filling.toppings[current].price) // 50
-                console.log(filling.toppings[current].calories) // 100
-                // console.log(hamburger1.myToppings.prototype = new Object({
-                // 	current: filling.toppings[current]
-                // }))
-                
+                // console.log(filling.toppings[current].price) // 50
+                // console.log(filling.toppings[current].calories) // 100
+                // // console.log(hamburger1.myToppings.prototype = new Object({
+                // // 	current: filling.toppings[current]
+                // // }))
+
                 someTempObj.price = filling.toppings[current].price;
                 someTempObj.calories = filling.toppings[current].calories;
                 someObj[current] = someTempObj;
                 someTempObj = {};
-                
+                // console.log(someObj)
                 hamburger1.myToppings = someObj;
-                console.log(hamburger1.myToppings);
+                console.log(hamburger1.myToppings[current].price);
                 console.log(hamburger1);
 
               }
@@ -119,18 +111,27 @@ document.addEventListener('DOMContentLoaded', () => {
         })
       })
     }
-  }
-  class MenuRender extends Fillings {
-    constructor() {
-      super()
-    }
     render() {
-      
+      const btn = document.querySelector('#button'),
+            element = document.createElement('div');
+
+      btn.addEventListener('click', (event) => {
+        event.preventDefault()
+        let result = 0
+        result += this.price + this.myToppings.price
+        element.innerHTML = `
+      			<div><b>Стоимость:</b> <span id="price">${this.price}</span> руб.</div>
+      			<div><b>Калорийность:</b> <span id="calories">${result}</span>ккал.</div>
+          `;
+        this.parent.append(element)
+      })
     }
   }
   const filling = new Fillings()
   filling.setChecked()
-  hamburger1.checkPrice()
-  console.log(someObj)
-  console.log(someTempObj)
+  filling.render()
+  // hamburger1.checkSize()
+  // hamburger1.checkPrice()
+  // console.log(someObj)
+  // console.log(someTempObj)
 });

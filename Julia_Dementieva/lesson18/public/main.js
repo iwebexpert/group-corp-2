777f63110123b7c74e56e7c55252005288151167
demjul <dementieva2@gmail.com>
2020-09-16 01:15:30 +0300

    lesson18 is ready

---
+++ Julia_Dementieva/lesson18/public/main.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector('.wrapper');
+wrapper.addEventListener('click', (event) => {
+    if(event.target.classList.contains('checkbox')){
+      if(event.target.checked){
+        saveCheckbox(event.target.id, true);
+      } else{
+        saveCheckbox(event.target.id, false);
+      };
+    } else if(event.target.classList.contains('btn-danger')) {
+        deleteTask(event.target.id);
+    };
+  });
+
+function saveCheckbox(id, status){
+  const newProduct = fetch(`/tasks/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      check:status,
+    }),
+    headers:{
+      'Content-type': 'application/json',
+    }
+  }).then( (res) => location.reload());
+}
+
+function deleteTask(id){
+  const newProduct =  fetch(`/tasks/${id}`, {
+    method: 'DELETE',
+  }).then( (res) => location.reload());
+}

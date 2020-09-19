addf9f1e1aa54cee0389202eab4a27e3decb9d1b
Arina <yakovlevaarischa@yandex.ru>
2020-09-19 18:26:03 +0300

    Update tasks.js

--- ArinaYakovleva/lesson19/models/tasks.js
+++ ArinaYakovleva/lesson19/models/tasks.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const taskSchema = new Schema({
     title: { type: String, required: true },
     completed: { type: Boolean, required: false, default: false },
-    date: { type: String, required: false, default: new Date().toDateString() }
+    date: { type: String, required: false, default: new Date().toLocaleString() }
 })
 
 

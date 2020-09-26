5d0d8fbc2a21e98ea46ef47db52ea684955f5dee
yong_god <yzn-yfv@mail.ru>
2020-09-26 11:23:19 +0300

    Removed unnecessary commented out code blocks

--- KrivonosAleksandr/react/lesson2/src/components/ChatArea.jsx
+++ KrivonosAleksandr/react/lesson2/src/components/ChatArea.jsx
@@ -71,28 +71,7 @@ export class ChatArea extends Component {
             <div className="chat">
                 {/********TITLE AREA*******/}
                 <TitleArea/>
-                {/*<div className="chat_title">*/}
-                {/*    <div className="chat_title__text">*/}
-                {/*        <div className="user_account">*/}
-                {/*            <a href="/#" className="user_account__imgLink">*/}
-                {/*                <div className="user_account__img" style={{background: `url(${botImg}) top/cover no-repeat`}}>*/}
-
-                {/*                </div>*/}
-                {/*            </a>*/}
-                {/*            <div className="user_account__info">*/}
-                {/*                <a href="/#" className="user_account__infoLink">Father Bot</a>*/}
-                {/*                <div className="state state_online">*/}
-                {/*                    <FontAwesomeIcon icon={faCircle}/>*/}
-                {/*                    Online*/}
-                {/*                </div>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*    <div className="chat_title__settings">*/}
-                {/*        <a href="/#" className="chat_title__settingsIcon icon_template"><FontAwesomeIcon icon={faCogs}/></a>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-
+                
                 <hr/>
 
                 {/********MESSAGES AREA*******/}
@@ -105,17 +84,6 @@ export class ChatArea extends Component {
                 <hr/>
 
                 {/********INPUT AREA*******/}
-                {/*<div className="input_area">*/}
-                {/*    <div className="input_area__actions">*/}
-                {/*        <a href="/#" className="attach_file icon_template"><FontAwesomeIcon icon={faPaperclip}/></a>*/}
-                {/*        <a href="/#" className="type_smile icon_template"><FontAwesomeIcon icon={faSmileWink}/></a>*/}
-                {/*        <a href="/#" className="attach_img icon_template"><FontAwesomeIcon icon={faImage}/></a>*/}
-                {/*    </div>*/}
-                {/*    <div className="message_area">*/}
-                {/*        <textarea name="sendMessage" className="message_send" placeholder="Напишите сообщение..."/>*/}
-                {/*        <a href="/#" className="message_send__btn"><FontAwesomeIcon icon={faPaperPlane}/></a>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
                 <InputArea onSend={this.handleMessageSend} userName={this.state.userName}/>
             </div>
         );

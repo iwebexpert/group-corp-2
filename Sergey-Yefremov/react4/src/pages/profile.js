284e38bea3a2e68596695df9156ae461966dd97b
Sergey <serzhik2036@gmail.com>
2020-10-04 23:45:46 +0300

    Homework-react4

---
+++ Sergey-Yefremov/react4/src/pages/profile.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function ProfilePage() {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>
+               Under Construction
+            </p>
+
+        </div>
+    )
+}

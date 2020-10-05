ee19eddd32ea82884f2db9ca2a61a3c1bbf5e977
Misha <mpeaches@live.com>
2020-10-05 10:20:46 +0300

    now for real

--- MykhailProchan/react4/src/components/ProfileTemplate.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export const Profile = () => {
-  return <div>Заглушка профиля, ведутся работы
-    <Link to='/' exact>Назад</Link>
-  </div>
-}

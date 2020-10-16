c35464b8fcbef1beb8d2654bd06c3908fa7ca7e5
demjul <dementieva2@gmail.com>
2020-10-16 17:33:35 +0300

    React1 class edit

--- Julia_Dementieva/react1/src/components/Header/Header.jsx
+++ Julia_Dementieva/react1/src/components/Header/Header.jsx
@@ -1,38 +1,43 @@
-import React from 'react';
-import {Avatar,  makeStyles} from '@material-ui/core';
+import React, { Component } from 'react'
+import {Avatar,  withStyles} from '@material-ui/core';
 import { deepOrange } from '@material-ui/core/colors';
 import { Link } from 'react-router-dom';
 
-
 import './Header.css';
 
-const useStyles = makeStyles({
+const styles = {
     root: {
-        backgroundColor: deepOrange[500], 
+        backgroundColor: deepOrange[500],
+        
       },
-});
+      large: {
+        width: '7px',
+        height: '7px',
+      },
+};
 
-export const Header = ({infoPerson, classes, push}) => {
+class HeaderClass extends Component {
 
-    const classes = useStyles();
-
-    const dispatch = useDispatch();
-
-    const handleRedirect = () => {
-        push('/');
+    handleRedirect = () => {
+        this.props.push('/');
     }
 
-    return (
-        (infoPerson) ?
-        (<div className="header">
-            {/* <Link to="/" style={{ textDecoration: 'none' }}><div className="header-logo">VChats</div></Link> */}
-            <div button="true" onClick={handleRedirect} style={{ cursor: 'pointer' }} className="header-logo">VChats</div>
-            <Link to="/About" style={{ textDecoration: 'none' }}>
-                <div className="header-avatar">
-                    <Avatar className={classes.root} src={infoPerson.avatar} />
-                    {infoPerson.name}
-                </div>
-            </Link>
-        </div>) : <div>Данные загружаются</div>
-    )
- }
+    render() {
+        const {infoPerson, classes} = this.props;
+        return (
+            (infoPerson) ?
+            (<div className="header">
+                {/* <Link to="/" style={{ textDecoration: 'none' }}><div className="header-logo">VChats</div></Link> */}
+                <div button="true" onClick={this.handleRedirect} style={{ cursor: 'pointer' }} className="header-logo">VChats</div>
+                <Link to="/About" style={{ textDecoration: 'none' }}>
+                    <div className="header-avatar">
+                        <Avatar className={classes.root} src={infoPerson.avatar} />
+                        {infoPerson.name}
+                    </div>
+                </Link>
+            </div>) : <div>Данные загружаются</div>
+        )
+    }
+}
+
+export const Header = withStyles(styles)(HeaderClass);

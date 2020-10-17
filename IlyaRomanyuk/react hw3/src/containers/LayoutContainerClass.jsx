85a78ef4df36a75f6eec7112bb1b2d4c619cb6f0
Ilya Romanyuk <ilya-romanyuk-1999@mail.ru>
2020-10-17 18:14:33 +0400

    homeworkReact8

--- IlyaRomanyuk/react hw3/src/containers/LayoutContainerClass.jsx
+++ IlyaRomanyuk/react hw3/src/containers/LayoutContainerClass.jsx
@@ -1,50 +1,38 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
 import { Layout } from '../components/Layout';
-import { addMessageTC, cleanAllMessagesAC, deleteMessageTC, chatsLoadTC } from '../actions/addChatAC';
+import { addMessageTC, deleteMessageTC, chatsLoadTC } from '../actions/addChatAC';
 
-class LayoutContainerClass extends React.Component {
-    componentDidMount = () => {
-        this.props.chatsLoadAction();
-    }
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-    handleMessageSend = async (message) => {
-        const { chatId, addMessageAction } = this.props;
-        addMessageAction(chatId, message)
+
+export const LayoutContainer = (props) => {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+
+    const chats = useSelector((state) => state.chats.data);
+    const messages = Object.values(chats).find(el => el.id == id) ? Object.values(chats).find(el => el.id == id) : null;
+
+    useEffect(() => {
+        dispatch(chatsLoadTC())
+    }, [])
+
+    const handleMessageSend = (message) => {
+        dispatch(addMessageTC(+id, message));
     };
 
-    render() {
-        const { messages, cleanAllMessagesAction, deleteMessageAction } = this.props;
-        return <Layout cleanAllMessagesAction={cleanAllMessagesAction}
-            deleteMessageAction={deleteMessageAction}
-            toggleIsFetching={this.toggleIsFetching}
-            chats={messages}
-            handleMessageSend={this.handleMessageSend} />;
-    }
-}
-
-const mapStateToProps = (state, ownProps) => {
-    const chats = state.chats.data;
-    const { match } = ownProps;
-
-    let messages = null;
-
-    if (match && chats[match.params.id]) {
-        messages = chats[match.params.id];
+    const deleteMessage = (id, messId) => {
+        dispatch(deleteMessageTC(id, messId))
     }
 
-    return {
-        chats,
-        messages,
-        chatId: match ? match.params.id : null
-    };
+    const cleanAll = (id) => {
+        messages.messages.forEach(async el => {
+            await dispatch(deleteMessageTC(id, el.id))
+        });
+    }
+
+    return <Layout cleanAllMessagesAction={cleanAll}
+        deleteMessageAction={deleteMessage}
+        chats={messages}
+        handleMessageSend={handleMessageSend} />;
 }
-
-const mapDispatchToProps = (dispatch) => ({
-    chatsLoadAction: () => dispatch(chatsLoadTC()),
-    addMessageAction: (chatId, message) => dispatch(addMessageTC(chatId, message)),
-    cleanAllMessagesAction: (chatId) => dispatch(cleanAllMessagesAC(chatId)),
-    deleteMessageAction: (chatId, messageId) => dispatch(deleteMessageTC(chatId, messageId))
-})
-
-export const LayoutContainer = connect(mapStateToProps, mapDispatchToProps)(LayoutContainerClass);

aea5b618261f54e157627789afbed1eb7600b81c
Shokarev Vladimir <shokarev.vl@gmail.com>
2020-10-17 22:21:09 +0300

    Homework8

---
+++ VladimirShokarev/React8/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderContainer } from "../containers/HeaderContainer";
+
+export function About() {
+    return (<>
+        <HeaderContainer />
+        <div className="about-container">
+            <h1 className="about-title">О нас</h1>
+        </div>
+    </>
+    )
+}
---
+++ VladimirShokarev/React8/webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.join(__dirname, 'src', 'components'),
+            pages: path.join(__dirname, 'src', 'pages'),
+            containers: path.join(__dirname, 'src', 'containers'),
+            actions: path.join(__dirname, 'src', 'actions'),
+            reducers: path.join(__dirname, 'src', 'reducers'),
+        },
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: '[name].[ext]',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-class-properties'],
+                    }
+                }
+            }
+        ],
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        historyApiFallback: true,
+        proxy: {
+            '/api': {
+              target: 'http://localhost:3000',
+              pathRewrite: {'^/api' : ''}
+            },
+          },
+    },
+};

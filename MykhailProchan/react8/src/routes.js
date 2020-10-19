aba0c3aac444391a1efe4fa225a63f5a71790d8a
Misha <mpeaches@live.com>
2020-10-19 06:30:57 +0300

    react8

---
+++ MykhailProchan/react8/src/routes.js
@@ -0,0 +1,20 @@
+import { Wrap } from 'components/Wrap'
+import { ProfileContainer } from './containers/ProfileContainer'
+
+export const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: Wrap
+  },
+  {
+    path: '/chats/:id([0-9]+)',
+    exact: true,
+    component: Wrap
+  },
+  {
+    path: '/profile',
+    exact: true,
+    component: ProfileContainer
+  }
+]

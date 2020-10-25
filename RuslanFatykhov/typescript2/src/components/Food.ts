71c1553478ef7d8cbbd7ff98d777d009e0fd1a18
Ruslan Fatykhov <rfatikhov@outlook.com>
2020-10-25 06:08:10 +0300

    typescript 2 homework

---
+++ RuslanFatykhov/typescript2/src/components/Food.ts
@@ -0,0 +1,47 @@
+import { Snake } from "./Snake";
+import { Board } from "./Board";
+import { Settings } from "./Settings";
+interface Coordinates {
+  x: number;
+  y: number;
+}
+
+export class Food {
+  public settings: Settings;
+  public snake: Snake;
+  public board: Board;
+  public x = 0;
+  public y = 0;
+
+  public constructor(settings: Settings, snake: Snake, board: Board) {
+    this.settings = settings;
+    this.snake = snake;
+    this.board = board;
+  };
+
+  public setNewFood(): void {
+    this.board.renderFood(this.randomCoords());
+  };
+
+  public randomCoords(): Coordinates {
+    while (true) {
+      this.x = Math.floor(Math.random() * this.settings.colsCount) + 1;
+      this.y = Math.floor(Math.random() * this.settings.rowsCount) + 1;
+      const coordinates: Coordinates = {
+        x: this.x,
+        y: this.y,
+      };
+      let cell: Element | null | undefined = this.board.getCell(coordinates);
+      if (cell) {
+        if (cell.classList.contains("snake")) {
+          continue;
+        };
+      };  
+      return coordinates;
+    };
+  };
+
+  public setFood(): void {
+    this.board.renderFood(this);
+  };
+};
---
+++ RuslanFatykhov/typescript2/src/components/Menu.ts
@@ -0,0 +1,18 @@
+export class Menu {
+  public startBtn: HTMLButtonElement | null;
+  public pauseBtn: HTMLButtonElement | null;
+  public constructor() {
+    this.startBtn = document.querySelector("#start-btn");
+    this.pauseBtn = document.querySelector("#pause-btn");
+  };
+
+  public addButtonsClickListeners(
+    startsBtn: () => void,
+    pausesBtn: () => void
+  ): void {
+    if (this.startBtn && this.pauseBtn) {
+      this.startBtn.addEventListener("click", startsBtn);
+      this.pauseBtn.addEventListener("click", pausesBtn);
+    };
+  };
+};

a7717c488ed47c3556f827d8778bc133bf141e2b
Unsleeping <mandrov.ad17@physics.msu.ru>
2020-10-28 21:25:01 +0300

    fix defaultProps issue

--- AlexanderMandrov/TypeScript/messenger/src/components/Error/Error.tsx
+++ AlexanderMandrov/TypeScript/messenger/src/components/Error/Error.tsx
@@ -10,32 +10,24 @@ type Props = {
   mx?: string;
 };
 
-export const Error: React.FC<Props> = ({ handleClick, mx }) => {
+const defaultHandleClick = (dispatch: React.Dispatch<any>) =>
+  dispatch(fetchProfileInfo('yellso'));
+
+export const Error: React.FC<Props> = ({
+  handleClick = defaultHandleClick,
+  mx = 'auto',
+}) => {
   const dispatch = useDispatch();
   return (
     <Box mx={mx} my="auto">
       Something went wrong...
-      {handleClick && (
-        <IconButton
-          color="secondary"
-          size="medium"
-          onClick={() => handleClick(dispatch)}
-        >
-          <Replay />
-        </IconButton>
-      )}
+      <IconButton
+        color="secondary"
+        size="medium"
+        onClick={() => handleClick(dispatch)}
+      >
+        <Replay />
+      </IconButton>
     </Box>
   );
 };
-
-/**
- * defaultProps doesn't work yet for FC: https://github.com/microsoft/TypeScript/issues/31247
- * so, here handleClick flag on line 18 is .-.
- *
- * check the https://github.com/Unsleeping/group-corp-2/blob/default-props-issue/AlexanderMandrov/TypeScript/messenger/src/components/Error/Error.tsx
- * for original version which won't contains any errors after fixing that issue
- */
-Error.defaultProps = {
-  handleClick: (dispatch) => dispatch(fetchProfileInfo('yellso')),
-  mx: 'auto',
-} as Partial<Props>;
